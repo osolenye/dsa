7a90aba5aff346ed7efc40b48a4bc79ef0dc5ed6
bekzat raiimkulov <raiimkulov.bekzat@gmail.com>
2025-06-24 00:20:27 +0600

    graphs

--- recursive_biggest_num.py
+++ recursive_biggest_num.py
@@ -2,4 +2,4 @@ def get_biggest(arr):
     if len(list) == 2:
         return list[0] if list[0] > list[l] else list[l]
     sub_max = max(list[l:])
-    return list[0) if list[0] > sub_max else sub_max
+    return list[0] if list[0] > sub_max else sub_max
