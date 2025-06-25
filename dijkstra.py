edaa3c64a0facb51ea29cbd88918cd884319b3db
bekzat raiimkulov <raiimkulov.bekzat@gmail.com>
2025-06-25 17:25:38 +0600

    dijkstra

---
+++ dijkstra.py
@@ -0,0 +1,37 @@
+graph = {}
+
+graph['start'] = {}
+graph['start']['a'] = 6
+graph['start']['b'] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {} 
+
+infinity = float('inf')
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['in'] = 'None'
+
+processed = []
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbours = graph[node]
+    for n in neighbours.keys():
+        new_cost = cost + neighbours[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
